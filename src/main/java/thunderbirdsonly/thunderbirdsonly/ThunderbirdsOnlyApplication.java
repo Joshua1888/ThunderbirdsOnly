1dd7953443825ab48e8ea20ce2ce7374b9287213
Joshua1888 <100439966+Joshua1888@users.noreply.github.com>
2024-12-31 20:41:48 -0800

    Basic Setup

---
+++ src/main/java/thunderbirdsonly/thunderbirdsonly/ThunderbirdsOnlyApplication.java
@@ -0,0 +1,13 @@
+package thunderbirdsonly.thunderbirdsonly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThunderbirdsOnlyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThunderbirdsOnlyApplication.class, args);
+    }
+
+}
